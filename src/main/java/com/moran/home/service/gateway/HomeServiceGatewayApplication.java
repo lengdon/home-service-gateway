f08d3c69dc5235192aaa39e0e7d7d4a632e560d6
Rahul <raahool_16@rediffmail.com>
2018-07-05 15:06:15 +0530

    Initial commit

---
+++ src/main/java/com/moran/home/service/gateway/HomeServiceGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.moran.home.service.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServiceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServiceGatewayApplication.class, args);
+	}
+}
